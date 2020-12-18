a6298502ce2bdb124884c1b77ab396703656928a
nton <antonm1.manzhos@gmail.com>
2020-12-18 21:07:23 +0200

    project init

---
+++ src/main/java/relatime/challenge/RelaTimeChallengeApplication.java
@@ -0,0 +1,13 @@
+package relatime.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaTimeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelaTimeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/relatime/challenge/RelaTimeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package relatime.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaTimeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
